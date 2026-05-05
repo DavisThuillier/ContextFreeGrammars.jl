12f8a56c44becf6194d8cac84c397a0b22239f86
Documenter.jl <documenter@juliadocs.github.io>
2026-05-05 20:30:58 +0000

    build based on 3818cdc

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"category":"section","location":"#ContextFreeGrammars","page":"Home","text":"Documentation for ContextFreeGrammars.\n\n","title":"ContextFreeGrammars"}]
+[{"category":"section","location":"api/#API-Reference","page":"API Reference","text":"","title":"API Reference"},{"category":"section","location":"api/#Semirings","page":"API Reference","text":"","title":"Semirings"},{"category":"section","location":"#ContextFreeGrammars","page":"Home","text":"Documentation for ContextFreeGrammars.\n\n","title":"ContextFreeGrammars"}]
 }
