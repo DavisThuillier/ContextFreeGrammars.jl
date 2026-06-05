2787d4442d7c43bd44c382af9251c658e7f2d8d4
DavisThuillier <dsthuillier@gmail.com>
2026-06-05 15:01:30 -0700

    Abstracted parse chart and added inside value calculation

--- src/parsers.jl
+++ src/parsers.jl
@@ -1,17 +1,32 @@
 tri(n) = (n * (n - 1)) ÷ 2
 @inline tri_index(i, j) = tri(j) + i + 1
 
-struct Chart{E}
+abstract type AbstractParseChart{N,T,E} end
+
+nonterminals(chart::AbstractParseChart) = chart.nonterminals
+input(chart::AbstractParseChart) = chart.input
+start_index(chart::AbstractParseChart) = chart.start
+start(chart::AbstractParseChart) = nonterminals(chart)[start_index(chart)]
+
+struct CYKParseChart{N, T, E} <: AbstractParseChart{N, T, E}
     vals::Matrix{E}
-    n::Int # String length
+    nonterminals::Vector{N}
+    input::Vector{T}
+    start::Int # Index of start symbol in nonterminals
 end
 
-Chart{E}(n::Int, m::Int) where {E} = Chart(zeros(E, tri(n + 1), m), n)
+function CYKParseChart{N, T, E}(input::AbstractVector{T}, nonterminals::AbstractVector{N}, S::N) where {N, T, E}
+    n = length(input)
+    start = findfirst(isequal(S), nonterminals)
+    return CYKParseChart(zeros(E, tri(n + 1), length(nonterminals)), nonterminals, input, start)
+end
 
-function Base.setindex!(c::Chart{E}, val::E, i::Int, j::Int, k::Int) where {E}
+CYKParseChart{N, T, E}(input::AbstractVector{T}, nonterminals::Set{N}, S::N) where {N, T, E} = CYKParseChart(input, collect(nonterminals), S)
+
+function Base.setindex!(c::CYKParseChart{N, T, E}, val::E, i::Int, j::Int, k::Int) where {N, T, E}
     c.vals[tri_index(i, j), k] = val
 end
-Base.getindex(c::Chart{E}, i::Int, j::Int, k::Int) where {E} = c.vals[tri_index(i, j), k]
+Base.getindex(c::CYKParseChart, i::Int, j::Int, k::Int) = c.vals[tri_index(i, j), k]
 
 # Goodman, semiring parsing
 
@@ -21,7 +36,6 @@ function cyk(input::AbstractVector{T}, grammar::AbstractGrammar{N,T,E}) where {N
 
     V = collect(nonterminals(grammar)) # Make indexed vector of symbols
     Σ = collect(terminals(grammar)) # Make indexed vector of symbols
-    m = length(V)
 
     terminal_productions = filter(is_terminal_production, productions(grammar))
     binary_productions   = filter(x -> !is_terminal_production(x), productions(grammar))
@@ -43,10 +57,10 @@ function cyk(input::AbstractVector{T}, grammar::AbstractGrammar{N,T,E}) where {N
         binary_productions
     )
 
-    chart = Chart{E}(n, m) # Look-up table for nonterminal associated to terminal production
+    chart = CYKParseChart{N, T, E}(input, V, start(grammar)) # Look-up table for inside values
 
     for i ∈ 1:n
-        for (A, a, w) ∈ terminal_idx_weights
+        for (A, a, w) ∈ terminal_idx_weights # A -> a
             if input[i] == Σ[a]
                 chart[i-1, i, A] += w
             end
@@ -64,11 +78,51 @@ function cyk(input::AbstractVector{T}, grammar::AbstractGrammar{N,T,E}) where {N
         end
     end
     
-
     return chart
 end
 
-function cyk(sentence::String, grammar::AbstractGrammar; dlm = " ")
-    words = String.(split(sentence, dlm))
+function cyk(input::String, grammar::AbstractGrammar; dlm = " ")
+    words = String.(split(input, dlm))
     return cyk(words, grammar)
+end
+
+function findfirstsub(sub::AbstractVector, seq::AbstractVector)
+    n, m = length(seq), length(sub)
+    m == 0 && return 1:0
+    for i in 1:(n - m + 1)
+        @views seq[i:i+m-1] == sub && return (i,i+m-1)
+    end
+    return nothing
+end
+
+inside(i::Int, j::Int, k::Int, chart::CYKParseChart{N, T, E}) where {N, T, E} = val(chart[i, j, k])
+
+function inside(i::Int, j::Int, C::N, chart::AbstractParseChart{N, T, E}) where {N, T, E}
+    k = findfirst(isequal(C), nonterminals(chart)) # Column index of symbol C
+    return inside(i, j, k, chart)
+end
+
+function inside(subsequence::AbstractVector{T}, C::N, chart::AbstractParseChart{N, T, E}) where {N, T, E}
+    span = findfirstsub(subsequence, input(chart))
+    isnothing(span) && throw(ArgumentError("subsequence not found in input"))
+    return inside(span[1] - 1, span[2], C, chart)
+end
+
+function inside(substring::AbstractString, C::N, chart::AbstractParseChart{N, T, E}; dlm = " ") where {N, T, E}
+    words = String.(split(substring, dlm))
+    return inside(words, C, chart)
+end
+
+function inside(subsequence::AbstractVector{T}, C::N, input::AbstractVector{T}, grammar::AbstractGrammar{N,T,E}) where {N,T,E}
+    chart = cyk(input, grammar)
+    return inside(subsequence, C, chart)
+end
+
+function val(chart::AbstractParseChart{N,T,E}) where {N, T, E}
+    return inside(0, length(input(chart)), start_index(chart), chart)
+end
+
+function val(input::AbstractVector{T}, grammar::AbstractGrammar{N, T, E}; algorithm = cyk) where {N, T, E}
+    chart = algorithm(input, grammar)
+    return inside(0, length(input(chart)), start_index(chart))
 end
